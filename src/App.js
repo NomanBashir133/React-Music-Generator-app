a72cc02bf2da1b6b86bef4439473b1d875fcc9f3
Noman Bashir <noman.bashir133@outlook.com>
2024-06-08 17:55:51 +0500

    minor improvements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Fragment } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from './layout/Header';
 import NavBar from './layout/NavBar';
 import Library from './Library/Library';
 import './App.css';
 
 const routes = [
-  { path: '/songs', component: Library, label: 'Library' },
+  { path: '/songs', component: Library, label: 'Songs' },
   { path: '/playlists', component: () => (<div>Playlists placeholder</div>), label: 'Playlist' },
   { path: '/followers', component: () => (<div>Followers placeholder</div>), label: 'Followers' },
 ];
@@ -23,6 +23,10 @@ function App() {
           {routes.map((route) => (
             <Route key={route.path} path={route.path} component={route.component} />
           ))}
+          <Route
+              path="*"
+              element={<Navigate to={routes[0].path} replace />}
+          />
         </Routes>
       </BrowserRouter>
     </Fragment>
